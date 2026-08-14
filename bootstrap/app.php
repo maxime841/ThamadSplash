0db182342b79474c5aa9d155bbfa39c264eb2f1d
maxime841 <maxime.poirot@gmail.com>
2026-08-15 00:02:26 +0200

    fix: register Inertia middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
 
         $middleware->alias([
             'admin' => AdminMiddleware::class,
