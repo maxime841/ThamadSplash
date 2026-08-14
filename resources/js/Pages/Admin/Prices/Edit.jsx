07792bcf10aa044f908e70f7ee78efd7a50c691d
maxime841 <maxime.poirot@gmail.com>
2026-08-14 20:12:38 +0200

    fix: correct PriceForm import

--- resources/js/Pages/Admin/Prices/Edit.jsx
+++ resources/js/Pages/Admin/Prices/Edit.jsx
@@ -2,15 +2,40 @@ import AdminLayout from "../../../Layouts/AdminLayout";
 import PageLayout from "../../../Components/Admin/Layout/PageLayout";
 import PriceForm from "./PriceForm";
 
+
+import { router } from "@inertiajs/react";
+import { route } from "../../../lib/route";
+
 export default function Edit({ price }) {
+
     return (
+
         <AdminLayout>
+
             <PageLayout
                 title="Modifier le tarif"
-                description={price.title}
+                description="Modifier les informations du tarif."
             >
-                <PriceForm price={price} />
+
+                <PriceForm
+                    price={price}
+                    submitLabel="Enregistrer"
+                    onSubmit={(data) =>
+
+                        router.post(
+                            route("admin.prices.update", price.slug),
+                            {
+                                ...data,
+                                _method: "put",
+                            }
+                        )
+
+                    }
+                />
+
             </PageLayout>
+
         </AdminLayout>
+
     );
 }
