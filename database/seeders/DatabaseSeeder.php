e3080ab6372a38b6b789bd9759b08cb4f3344ac0
maxime841 <maxime.poirot@gmail.com>
2026-08-14 23:06:01 +0200

    feat: add super admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'role' => 'Administrateur'
-        ]);
+        User::updateOrCreate(
+            [
+                'email' => env('ADMIN_EMAIL'),
+            ],
+            [
+                'name' => 'Maxime',
+                'password' => Hash::make(env('ADMIN_PASSWORD')),
+                'role' => 'super-admin',
+            ]
+        );
     }
 }
+
